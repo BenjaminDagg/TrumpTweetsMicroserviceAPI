6c52dc347647296284b5b8e463c05ccc86c799a3
BenjaminDagg <bdagg@csumb.edu>
2018-04-05 23:02:29 -0700

    added CORS to tweets api

--- microservices/src/main/java/com/example/microservices/tweets/web/TweetWeb.java
+++ microservices/src/main/java/com/example/microservices/tweets/web/TweetWeb.java
@@ -1,12 +1,14 @@
 package com.example.microservices.tweets.web;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.microservices.tweets.logic.TweetsLogicManaget;
 
+@CrossOrigin
 @RestController
 public class TweetWeb {
 	
