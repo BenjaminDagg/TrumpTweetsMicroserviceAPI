72e07fb1a7c018eeaad0632fbffd1d76a1f13db2
BenjaminDagg <bdagg@csumb.edu>
2018-04-05 21:32:50 -0700

    added CORS for trump route

--- microservices/src/main/java/com/example/microservices/trump/web/TrumpWeb.java
+++ microservices/src/main/java/com/example/microservices/trump/web/TrumpWeb.java
@@ -2,12 +2,14 @@ package com.example.microservices.trump.web;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.microservices.trump.logic.TrumpLogicManager;
 
+@CrossOrigin
 @RestController
 public class TrumpWeb {
 	
